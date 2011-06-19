b38922e20729ed3edbb838b459ff25023708d93c
Steve Streza <stevestreza@gmail.com>
2011-06-19 13:54:36 -0700

    Major project restructure

---
+++ MWNetworkingTests/MWNetworkingTests.h
@@ -0,0 +1,17 @@
+//
+//  MWNetworkingTests.h
+//  MWNetworkingTests
+//
+//  Created by Steve Streza on 6/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface MWNetworkingTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ MWNetworkingTests/MWNetworkingTests.m
@@ -0,0 +1,33 @@
+//
+//  MWNetworkingTests.m
+//  MWNetworkingTests
+//
+//  Created by Steve Streza on 6/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MWNetworkingTests.h"
+
+
+@implementation MWNetworkingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STAssertEquals(42, 42, @"42 is 42");
+}
+
+@end
